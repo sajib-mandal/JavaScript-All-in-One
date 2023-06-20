f46e0af4af4c4559e3d9f8a246bd71dbb7d3e5b6
sajib-mandal <sajibmandalcse@gmail.com>
2023-06-20 17:15:28 +0530

    images folder added

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,9 +1,1 @@
 'use strict';
-
-let element = document.querySelector('.message');
-element.textContent = "New text content";
-console.log(element.textContent)
-
-// console.log(document.getElementById('message').textContent);
-
-// console.log(document.getElementsByClassName("message").innerHTML)
